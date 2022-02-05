dbfb033c17dab20bdf7e8bc3d63c41af0d12c7ac
rahulchinchole <rahulchinchole487@gmail.com>
2022-02-05 15:33:16 +0530

    Modified cards

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -3,7 +3,7 @@ import React from "react";
 const Searchbox = () => {
   return (
     <div className="pa2">
-      <input className="mw-100 w-100 w5-ns f5 input-reset ba b--black-20 pv3 ph4 border-box bg-light-green" type="search" placeholder="Search robots"></input>
+      <input className="mw-100 w-100 w5-ns f4 input-reset ba b--black-20 pv3 ph4 border-box bg-light-green" type="search" placeholder="Search robots"></input>
     </div>
   );
 };
